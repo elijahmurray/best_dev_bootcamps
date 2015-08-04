c15bebb945e914a93fcdab5e77af5eea394f718b
Elijah Murray <elijahmurray@gmail.com>
2015-08-04 09:06:03 -0700

    setup bootcamps spec

---
+++ spec/controllers/bootcamps_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe BootcampsController, type: :controller do
+  describe "GET #index" do
+    pending
+  end
+
+  describe "GET #new" do
+    pending
+  end
+  
+  describe "GET #show" do
+    pending
+  end
+
+  describe "POST? #create" do
+    pending
+  end
+
+  describe "GET #edit" do
+    pending
+  end
+
+  describe "PUSH #edit" do
+    pending
+  end
+  
+  describe "#update" do
+    pending
+  end
+
+end
