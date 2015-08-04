de11be0ff7ff8df89be1d49326b44acd23cc4422
Elijah Murray <elijahmurray@gmail.com>
2015-08-04 09:00:01 -0700

    passing tests for static_pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,13 +1,35 @@
 require 'rails_helper'
 
 describe StaticPagesController, type: :controller do
-  describe 'GET #home' do
-    pending
+  render_views
 
+  describe 'GET #home' do
+    before(:each) do
+      get :home
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "says 'Welcome'" do
+      expect(response.body).to match /Welcome/
+    end
   end
 
   describe 'GET #about' do
-    pending
+    before(:each) do
+      get :about
+    end
 
+    it "respondds successfully with an HTTP 200 status code" do
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "says 'About Us'" do
+      expect(response.body).to match /About Us/
+    end
   end
 end
