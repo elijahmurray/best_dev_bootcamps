4c8cfebd6e7d0c9ec62182523b1c45d8bddfd76a
Elijah Murray <elijahmurray@gmail.com>
2015-08-04 08:50:50 -0700

    clean up more minitest and create structure for static pages controller tests

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe StaticPagesController, type: :controller do
+  describe 'GET #home' do
+    pending
+
+  end
+
+  describe 'GET #about' do
+    pending
+
+  end
+end
